1266f98b30a913bbed0aa0777711ca78400a0b23
sukandawiguna <sukandawiguna@mail.unpas.ac.id>
2019-04-08 22:42:44 +0700

    first commit

---
+++ Beginners/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Beginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VeriablesandConstants/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VeriablesandConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Dudung";
+            var isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.ReadKey();
+        }
+    }
+}
